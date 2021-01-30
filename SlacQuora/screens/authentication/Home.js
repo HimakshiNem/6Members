8316b016fdb6e16faa9278bc68f1c2507c3930a2
gsheng19 <litekirax3@gmail.com>
2021-01-31 00:49:03 +0800

    Home.js
    Styled Buttons, Added Fonts

--- SlacQuora/screens/authentication/Home.js
+++ SlacQuora/screens/authentication/Home.js
@@ -8,33 +8,41 @@ import {
     TextInput,
     TouchableOpacity,
   } from "react-native";
+import { ComicNeue_400Regular } from '@expo-google-fonts/dev';
 
 function Home(props) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={styles.homeText}>Home Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.homeText}>I AM A:{"\n\n\n"}</Text>
             <TouchableOpacity
-                    style={styles.button}>
+                    style={styles.button} onPress={() => props.navigation.navigate('SignUp')}>
                 <Icon
                     reverse
-                    name='account-tie'
-                    type='material-community'
+                    name='chalkboard-teacher'
+                    type='font-awesome-5'
+                    color='#FF9801'
+
+                />
+                <Text 
+                    style={styles.bText}>
+                        Professor
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                    style={styles.button} onPress={() => props.navigation.navigate('SignUp')}>
+                <Icon
+                    reverse
+                    name='user-graduate'
+                    type='font-awesome-5'
                     color='#FF9801'
                 />
                 <Text 
                     style={styles.bText}>
-                        Professors
+                        Student
                 </Text>
             </TouchableOpacity>
             
-            <Button
-                title="Professor "
-                onPress={() => props.navigation.navigate('SignUp')}
-            />
-            <Button
-                title="Student "
-                onPress={() => props.navigation.navigate('SignUp')}
-            />
         </View>
     );
 }
@@ -43,23 +51,32 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: "#fff",
+      justifyContent: 'center',
+      alignItems: 'center'
     },
 
     homeText: {
-
+        fontSize: 20,
+        fontFamily: "ComicNeue_400Regular",
     },
 
     button: {
         alignItems: "center",
+        width: 240,
         borderColor: "#FF9801",
         borderWidth: 1,
         borderRadius: 5, 
-        backgroundColor: "#FFFFF",
+        backgroundColor: "#FF9801",
         paddingRight: 50,
         paddingLeft: 50,
         paddingTop: 50,
         paddingBottom: 50,
         marginBottom: 20,
+      },
+
+      bText:{
+        color: "#FFFFFF",
+        fontSize: 25,
       },
 });
 
