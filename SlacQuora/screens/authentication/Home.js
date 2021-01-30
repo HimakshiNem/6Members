60bc0e1c9e974930d5f303ee475c7d9921b35149
th3pho3nix <hisyam001@e.ntu.edu.sg>
2021-01-30 23:23:24 +0800

    Updated Home.js

--- SlacQuora/screens/authentication/Home.js
+++ SlacQuora/screens/authentication/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Icon } from 'react-native-elements'
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    TextInput,
+    TouchableOpacity,
+  } from "react-native";
 
 function Home(props) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+            <Text style={styles.homeText}>Home Screen</Text>
+            <TouchableOpacity
+                    style={styles.button}>
+                <Icon
+                    reverse
+                    name='account-tie'
+                    type='material-community'
+                    color='#FF9801'
+                />
+                <Text 
+                    style={styles.bText}>
+                        Professors
+                </Text>
+            </TouchableOpacity>
+            
             <Button
                 title="Professor "
                 onPress={() => props.navigation.navigate('SignUp')}
@@ -16,5 +38,29 @@ function Home(props) {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+    },
+
+    homeText: {
+
+    },
+
+    button: {
+        alignItems: "center",
+        borderColor: "#FF9801",
+        borderWidth: 1,
+        borderRadius: 5, 
+        backgroundColor: "#FFFFF",
+        paddingRight: 50,
+        paddingLeft: 50,
+        paddingTop: 50,
+        paddingBottom: 50,
+        marginBottom: 20,
+      },
+});
 
 export default Home;
